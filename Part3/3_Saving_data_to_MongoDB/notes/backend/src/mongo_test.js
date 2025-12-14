930f17b190c28e9f3abd8319ca5a8f852d8903d6
rezvan.aj <rezvan.aj@klarna.com>
2025-12-14 20:56:18 +0100

    Part3-3_Saving_data_to_MongoDB

---
+++ Part3/3_Saving_data_to_MongoDB/notes/backend/src/mongo_test.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+console.log(password)
+const databaseName = `notesApp`
+
+const url = `mongodb+srv://fullstack:${password}@cluster0.oshkocc.mongodb.net/${databaseName}?retryWrites=true&w=majority&appName=Cluster0`
+
+mongoose.set('strictQuery',false)
+
+mongoose.connect(url, { family: 4 })
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+})
+
+const Note = mongoose.model('Note', noteSchema)
+
+
+// const note = new Note({
+//   content: 'HTML is easy',
+//   important: true,
+// })
+
+// note.save().then(result => {
+//   console.log('note saved!')
+//   mongoose.connection.close()
+// })
+
+Note.find({}).then(result => {
+    result.forEach(note => {
+      console.log(note)
+    })
+    mongoose.connection.close()
+  })
